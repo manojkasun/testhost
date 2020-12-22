dd910ad936df06b8bb60b204bac80e71cabf482d
ManojPriyaKasun <33677026+manojkasun@users.noreply.github.com>
2020-12-22 20:48:47 +0530

    dfg

--- components/button/dist/bit-button.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './button.module';
-export { ButtonComponent as ɵa } from './button.component';
--- components/togglebutton/dist/togglebutton/esm5/public_api.js
+++
@@ -1,6 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-export { TOGGLEBUTTON_VALUE_ACCESSOR, ToggleButton, ToggleButtonModule } from './togglebutton';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljX2FwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL0BiaXQvdG9nZ2xlYnV0dG9uLyIsInNvdXJjZXMiOlsicHVibGljX2FwaS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7O0FBQUEsOEVBQWMsZ0JBQWdCLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tICcuL3RvZ2dsZWJ1dHRvbic7Il19
