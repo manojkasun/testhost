1d7b74d063fb1e54f8576dc9842bb465d1983bf0
ManojPriyaKasun <33677026+manojkasun@users.noreply.github.com>
2020-12-22 18:48:05 +0530

    dfgdadd remove bit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,8 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { ProductsComponent } from './products/products.component';
 import { AddProductComponent } from './add-product/add-product.component';
-import { ButtonModule } from '@bit/heyitsgeo.ng-semantic-ui.button';
-import { StyleLinksModule } from '@bit/heyitsgeo.ng-semantic-ui.style-links';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BtnModule } from '@bit/austinhenderson19.mobile-components.btn';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,9 +19,6 @@ import { BtnModule } from '@bit/austinhenderson19.mobile-components.btn';
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    StyleLinksModule,
-    BtnModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule
